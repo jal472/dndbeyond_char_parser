3be07b2ba4c8da2ff88f862d096178ce3f12874a
Jack LoCasto <jacklocasto@gmail.com>
2025-05-30 22:24:58 -0400

    Expanded Character's baseInfo by creating a nested class to encapsulate this data and update DnDBeyondParser to account for this.

--- src/main/java/com/jal472/app/model/Character.java
+++ src/main/java/com/jal472/app/model/Character.java
@@ -1,30 +1,55 @@
 package com.jal472.app.model;
 
 public class Character {
-    private String name;
-    private String race;
-    private String characterClass;
-    private int level;
+    private CharacterBaseInfo baseInfo;
+    // (stats.n.value) - 1 strength, 2 dexterity, 3 constitution, 4 intelligence, 5 wisdom, 6 charisma
+//    private CharacterStats;
+    // acrobatics, animalHandling, arcana, athletics, deception, history, insight, intimidation, investigation, medicine,
+    // nature, perception, performance, persuasion, religion, sleightOfHand, stealth, survival
+//    private CharacterSkills;
+    // strengthSavingThrowModifiers[] - list of potential strength saving throw modifiers
+//    private CharacterSavingThrowMods;
+    // customSenses[] - senses[] - list of potential senses
+//    private CharacterSenses;
+    // customProficiencies[] - armor[], weapons[], tools[], languages[]
+//    private CharacterProficiencies;
+    // inventory (n.definition.name)
+//    private CharacterInventory;
     /**
      * Character class constructor
-     * @param name character's name
-     * @param race character's race
-     * @param characterClass character's class
-     * @param level character's level
+     * @param baseInfo character's baseInfo - see CharacterBaseInfo nested class
      */
-    public Character(String name, String race, String characterClass, int level) {
-        this.name = name;
-        this.race = race;
-        this.characterClass = characterClass;
-        this.level = level;
+    public Character(CharacterBaseInfo baseInfo) {
+        this.baseInfo = baseInfo;
     }
-
+    /**
+     * Overwritten toString method to print the Character nicely
+     * @return string describing the Character object
+     */
+    @Override
+    public String toString() {
+        return baseInfo.toString();
+    }
+    /**
+     *
+     * @return
+     */
+    public CharacterBaseInfo getBaseInfo() {
+        return baseInfo;
+    }
+    /**
+     *
+     * @param baseInfo
+     */
+    public void setBaseInfo(CharacterBaseInfo baseInfo) {
+        this.baseInfo = baseInfo;
+    }
     /**
      * Character name getter
      * @return character's name
      */
     public String getName() {
-        return name;
+        return baseInfo.getName();
     }
 
     /**
@@ -32,7 +57,7 @@ public class Character {
      * @return character's race
      */
     public String getRace() {
-        return race;
+        return baseInfo.getRace();
     }
 
     /**
@@ -40,7 +65,7 @@ public class Character {
      * @return character's class
      */
     public String getCharacterClass() {
-        return characterClass;
+        return baseInfo.getCharacterClass();
     }
 
     /**
@@ -48,16 +73,182 @@ public class Character {
      * @return character's level
      */
     public int getLevel() {
-        return level;
+        return baseInfo.getLevel();
     }
 
     /**
-     * Overwritten toString method to print the Character nicely
-     * @return string describing the Character object
+     * Object that encapsulates the character base info
      */
-    @Override
-    public String toString() {
-        return "\tname: " + name + "\n\tlevel: " + level + "\n\trace: " + race + "\n\tclass: " + characterClass;
-    }
+    public static class CharacterBaseInfo {
+        private String name; // already have
+        private int level; // already have
+        private String species; // what race is
+        private String race; // options.race.definition.name
+        private String characterClass; // already have
+        private String characterSubclass; // subclassDefinition.name
+        private String background; // background.name
+        private int baseHp; // baseHitPoints
+        private int bonusHp; // bonusHitPoints
+        private int xp; // currentXp
+        private int walkingSpeed; // weightSpeeds.normal.walk
+        private boolean heroicInspiration; // inspiration
+        private int gp; // currencies.gp
 
+        public CharacterBaseInfo(String name, int level, String species, String race, String characterClass,
+                                 String characterSubclass, String background, int baseHp, int bonusHp, int xp,
+                                 int walkingSpeed, boolean heroicInspiration, int gp) {
+            this.name = name;
+            this.level = level;
+            this.species = species;
+            this.race = race;
+            this.characterClass = characterClass;
+            this.characterSubclass = characterSubclass;
+            this.background = background;
+            this.baseHp = baseHp;
+            this.bonusHp = bonusHp;
+            this.xp = xp;
+            this.walkingSpeed = walkingSpeed;
+            this.heroicInspiration = heroicInspiration;
+            this.gp = gp;
+        }
+        @Override
+        public String toString() {
+            return "\tname: " + name +
+                    "\n\tlevel: " + level +
+                    "\n\tspecies: " + species +
+                    "\n\trace: " + race +
+                    "\n\tclass: " + characterClass +
+                    "\n\tsubclass: " + characterSubclass +
+                    "\n\tbackground: " + background +
+                    "\n\tbaseHp: " + baseHp +
+                    "\n\tbonusHp: " + bonusHp +
+                    "\n\txp: " + xp +
+                    "\n\twalkingSpeed: " + walkingSpeed + " ft" +
+                    "\n\theroicInspiration: " + heroicInspiration +
+                    "\n\tgp: " + gp;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getLevel() {
+            return level;
+        }
+
+        public void setLevel(int level) {
+            this.level = level;
+        }
+
+        public String getSpecies() {
+            return species;
+        }
+
+        public void setSpecies(String species) {
+            this.species = species;
+        }
+
+        public String getRace() {
+            return race;
+        }
+
+        public void setRace(String race) {
+            this.race = race;
+        }
+
+        public String getCharacterClass() {
+            return characterClass;
+        }
+
+        public void setCharacterClass(String characterClass) {
+            this.characterClass = characterClass;
+        }
+
+        public String getCharacterSubclass() {
+            return characterSubclass;
+        }
+
+        public void setCharacterSubclass(String characterSubclass) {
+            this.characterSubclass = characterSubclass;
+        }
+
+        public String getBackground() {
+            return background;
+        }
+
+        public void setBackground(String background) {
+            this.background = background;
+        }
+
+        public int getBaseHp() {
+            return baseHp;
+        }
+
+        public void setBaseHp(int baseHp) {
+            this.baseHp = baseHp;
+        }
+
+        public int getBonusHp() {
+            return bonusHp;
+        }
+
+        public void setBonusHp(int bonusHp) {
+            this.bonusHp = bonusHp;
+        }
+
+        public int getXp() {
+            return xp;
+        }
+
+        public void setXp(int xp) {
+            this.xp = xp;
+        }
+
+        public int getWalkingSpeed() {
+            return walkingSpeed;
+        }
+
+        public void setWalkingSpeed(int walkingSpeed) {
+            this.walkingSpeed = walkingSpeed;
+        }
+
+        public boolean isHeroicInspiration() {
+            return heroicInspiration;
+        }
+
+        public void setHeroicInspiration(boolean heroicInspiration) {
+            this.heroicInspiration = heroicInspiration;
+        }
+
+        public int getGp() {
+            return gp;
+        }
+
+        public void setGp(int gp) {
+            this.gp = gp;
+        }
+    }
+    // (stats.n.value) - 1 strength, 2 dexterity, 3 constitution, 4 intelligence, 5 wisdom, 6 charisma
+//    private CharacterStats;
+    // acrobatics, animalHandling, arcana, athletics, deception, history, insight, intimidation, investigation, medicine,
+    // nature, perception, performance, persuasion, religion, sleightOfHand, stealth, survival
+//    private CharacterSkills;
+    // strengthSavingThrowModifiers[] - list of potential strength saving throw modifiers
+//    private CharacterSavingThrowMods;
+    // customSenses[] - senses[] - list of potential senses
+    public static class CharacterSenses{
+
+    }
+    // customProficiencies[] - armor[], weapons[], tools[], languages[]
+    public static class CharacterProficiencies {
+
+    }
+    // inventory (n.definition.name)
+    public static class CharacterInventory {
+
+    }
 }
--- src/main/java/com/jal472/app/parser/DnDBeyondParser.java
+++ src/main/java/com/jal472/app/parser/DnDBeyondParser.java
@@ -13,6 +13,10 @@ public class DnDBeyondParser {
     private final DnDHttpClient httpClient;
     private static final String BASE_API = "https://character-service.dndbeyond.com/character/v5/character/";
 
+    /**
+     * DnDBeyondParser constructor to initialize the httpClient to access dndbeyond character pages
+     * @param httpClient the http client that makes requests to dndbeyond character pages
+     */
     public DnDBeyondParser(DnDHttpClient httpClient) {
         this.httpClient = httpClient;
     }
@@ -22,11 +26,6 @@ public class DnDBeyondParser {
      * @return a constructed Character object
      */
     public Character parseFromUrl(String url) {
-//        String name = "Test Name";
-//        String race = "Humanoid";
-//        String characterClass = "Sorcerer";
-//        int level = 1;
-
         try {
             String characterId = extractCharacterId(url);
             String json = httpClient.get(BASE_API + characterId);
@@ -36,7 +35,6 @@ public class DnDBeyondParser {
             return null;
         }
     }
-
     /**
      * Extracts the character id from the dnd beyond url
      * @param url dndbeyond character url
@@ -46,18 +44,34 @@ public class DnDBeyondParser {
         String[] parts = url.split("/");
         return parts[parts.length - 1];
     }
-
+    /**
+     * Parses the json body of the dndbeyond character page and creates the Character object
+     * @param json json body of the dndbeyond character page
+     * @return a constructed Character object
+     * @throws Exception
+     */
     private Character parseCharacterJson(String json) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         JsonNode root = mapper.readTree(json);
 
         JsonNode data = root.path("data");
-
+        // Base Info - Fix: subclass, background, walkingSpeed
         String name = data.path("name").asText("Unknown");
-        String race = data.path("race").path("fullName").asText("Unknown");
-        String charClass = data.path("classes").get(0).path("definition").path("name").asText("Unknown");
         int level = data.path("classes").get(0).path("level").asInt(0);
+        String species = data.path("race").path("fullName").asText("Unknown");
+        String race = data.path("options").path("race").get(0).path("definition").path("name").asText("Unknown");
+        String charClass = data.path("classes").get(0).path("definition").path("name").asText("Unknown");
+        String charSubclass = data.path("classes").get(0).path("subclassDefinition").path("name").asText("Unknown"); // subclassDefinition.name
+        String background = data.path("background").path("name").asText("Unknown"); // background.name
+        int baseHp = data.path("baseHitPoints").asInt(0); // baseHitPoints
+        int bonusHp = data.path("bonusHitPoints").asInt(0); // bonusHitPoints
+        int xp = data.path("currentXp").asInt(0);; // currentXp
+        int walkingSpeed = data.path("weightSpeeds").path("normal").path("walk").asInt(0);; // weightSpeeds.normal.walk
+        boolean heroicInspiration = data.path("inspriation").asBoolean(false); // inspiration
+        int gp = data.path("currencies").path("gp").asInt(0); // currencies.gp
 
-        return new Character(name, race, charClass, level);
+        Character.CharacterBaseInfo baseInfo = new Character.CharacterBaseInfo(name, level, species, race, charClass,
+                charSubclass, background, baseHp, bonusHp, xp, walkingSpeed, heroicInspiration, gp);
+        return new Character(baseInfo);
     }
 }
